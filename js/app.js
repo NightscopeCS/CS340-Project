7fe5a7a05457cb4fb7af0cc1d3609ce9cf9a4484
NightscopeCS <rhardin136@gmail.com>
2020-08-04 09:43:43 -0700

    Synced up

--- js/app.js
+++ js/app.js
@@ -1,36 +1,20 @@
-var express = require("express");
+$(document).ready(function() {
+ // executes when HTML-Document is loaded and DOM is ready
 
-var app = express();
+// breakpoint and up  
+$(window).resize(function(){
+	if ($(window).width() >= 980){	
 
-var handlebars = require("express-handlebars").create({defaultLayout:"main"});
-var bodyParser = require("body-parser");
-var cors = require("cors");
+      // when you hover a toggle show its dropdown menu
+      $(".navbar .dropdown-toggle").hover(function () {
+         $(this).parent().toggleClass("show");
+         $(this).parent().find(".dropdown-menu").toggleClass("show"); 
+       });
 
-module.exports.pool = pool;
-
-app.engine("handlebars", handlebars.engine);
-app.set("view engine", "handlebars");
-app.set("port", 1752);
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(express.static('public'));
-app.use(cors());
-
-// Error handling
-app.use(function(req,res){
-  res.status(404);
-  res.render("404");
+        // hide the menu when the mouse leaves the dropdown
+      $( ".navbar .dropdown-menu" ).mouseleave(function() {
+        $(this).removeClass("show");  
+      });
+	}	
+});  
 });
-
-app.use(function(err,req,res,next){
-  console.error(err.stack);
-  res.type("plain/text");
-  res.status(500);
-  res.render("500");
-});
-
-// Begins Express process and displays Host/Port
-app.listen(app.get("port"), function(){
-    console.log(`Express started on ${process.env.HOSTNAME}:${app.get("port")}; press Ctrl-C to terminate.`);
-  });
